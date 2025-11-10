a98360d9d28da304592191498b4fbddb80f75f8b
Viktoriia <struk.victoria@gmail.com>
2025-11-10 20:31:15 +0200

    chore: add dev-dist to .gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d2lpmst005g"
+    "revision": "0.rnl7gp3tce"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
