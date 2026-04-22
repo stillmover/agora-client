9e467444d9f987d3e97802424b71e0e227f65c98
Viktoriia <struk.victoria@gmail.com>
2026-04-22 23:54:43 +0300

    chore: update with docs

--- src/shared/api/gql/client.ts
+++ src/shared/api/gql/client.ts
@@ -1,8 +1,8 @@
-import { createClient, cacheExchange, fetchExchange } from "urql";
+import { Client, cacheExchange, fetchExchange } from "urql";
 import { env } from "@/shared/utils/env";
 import { errorExchange } from "./error-exchange";
 
-export const urqlClient = createClient({
+export const client = new Client({
   url: `${env.BACKEND_URL}/graphql`,
   exchanges: [cacheExchange, errorExchange, fetchExchange],
   fetchOptions: {
